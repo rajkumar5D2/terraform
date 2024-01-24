7decf59c4c6548ac24b0c54efd4f49f88c4717ab
rajkumar5d2 <rajkumarmikkili@gmail.com>
2024-01-24 22:10:03 +0000

    jenkins

--- roboshop-infra-standard/11-mysql/main.tf
+++ roboshop-infra-standard/11-mysql/main.tf
@@ -12,6 +12,7 @@ module "mysql_instance" {
 
 module "records" {
   source  = "terraform-aws-modules/route53/aws//modules/records"
+  version = "~> 2.0"
   zone_name = var.zone_name
      records = [{
       name    = "mysql"
